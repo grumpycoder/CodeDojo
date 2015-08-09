e634a01a077a1f572a8399306f4ca1ae05099d2a
Mark Lawrence <grumpycoder@outlook.com>
2015-08-09 00:03:35 -0500

    FizzBuzz test kata

---
+++ CodeDojo/CodeDojo.Tests/FizzBuzzTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodeDojo.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void ThreeIsDivisibleByThree()
+        {
+            var fb = new FizzBuzz();
+            bool divisible = FizzBuzz.IsDivisibleByThree(3);
+            Assert.That(divisible, Is.True);
+
+        }
+
+        [Test]
+        public void FiveIsDivisibleByFive()
+        {
+            var fb = new FizzBuzz();
+            bool divisible = FizzBuzz.IsDivisibleByFive(5);
+            Assert.That(divisible, Is.True);
+
+        }
+
+        [Test]
+        public void TextForDivisibleByThreeIsFizz()
+        {
+            var fb = new FizzBuzz();
+            var text = fb.GetTextForNumber(3);
+            Assert.That(text, Is.EqualTo("Fizz"));
+
+        }
+
+        [Test]
+        public void TextForDivisibleByFiveIsBuzz()
+        {
+            var fb = new FizzBuzz();
+            var text = fb.GetTextForNumber(5);
+            Assert.That(text, Is.EqualTo("Buzz"));
+
+        }
+
+        [Test]
+        public void TextForDivisibleByThreeAndFiveIsFizzBuzz()
+        {
+            var fb = new FizzBuzz();
+            var text = fb.GetTextForNumber(15);
+            Assert.That(text, Is.EqualTo("FizzBuzz"));
+
+        }
+
+        [Test]
+        public void TextForNotDivisibleByThreeOrFiveIsItselfText()
+        {
+            var fb = new FizzBuzz();
+            var text = fb.GetTextForNumber(1);
+
+            Assert.That(text, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void PrintTextForOneToFifteen()
+        {
+            var fb = new FizzBuzz();
+
+            var text = fb.Print(15);
+
+            Assert.That(text, Is.EqualTo("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz "));
+        }
+
+        [Test]
+        public void ShouldPrintNumbersOneToTwo()
+        {
+            var fb = new FizzBuzz();
+            var text = fb.Print(2);
+            Assert.That(text, Is.EqualTo("1 2 "));
+        }
+
+        
+    }
+
+    public class FizzBuzz
+    {
+    
+        public static bool IsDivisibleByThree(int input)
+        {
+            return input%3 == 0;
+        }
+
+        public static bool IsDivisibleByFive(int input)
+        {
+            return input%5 == 0;
+        }
+
+        public string GetTextForNumber(int input)
+        {
+            if (IsDivisibleByThree(input) && IsDivisibleByFive(input))
+                return "FizzBuzz";
+
+            if (IsDivisibleByThree(input))
+                return "Fizz";
+
+            if (IsDivisibleByFive(input))
+                return "Buzz";
+
+            return input.ToString();
+        }
+
+        public string Print(int input)
+        {
+            var sb = new StringBuilder();
+            for (int i = 1; i <= input; i++)
+            {
+                sb.AppendFormat("{0} ", GetTextForNumber(i));
+            }
+            return sb.ToString();
+        }
+    }
+}
