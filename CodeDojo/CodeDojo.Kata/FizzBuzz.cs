5f57a4a23e957890a64ce2dfa5d72f7c6f6776a9
Mark Lawrence <grumpycoder@outlook.com>
2015-08-09 15:11:39 -0500

    moved fizzbuzz class out of testting framework.

---
+++ CodeDojo/CodeDojo.Kata/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace CodeDojo.Kata
+{
+    public class FizzBuzz
+    {
+    
+        public static bool IsDivisibleByThree(int input)
+        {
+            return input%3 == 0;
+        }
+
+        public static bool IsDivisibleByFive(int input)
+        {
+            return input%5 == 0;
+        }
+
+        public string GetTextForNumber(int input)
+        {
+            if (IsDivisibleByThree(input) && IsDivisibleByFive(input))
+                return "FizzBuzz";
+
+            if (IsDivisibleByThree(input))
+                return "Fizz";
+
+            if (IsDivisibleByFive(input))
+                return "Buzz";
+
+            return input.ToString();
+        }
+
+        public string Print(int input)
+        {
+            var sb = new StringBuilder();
+            for (int i = 1; i <= input; i++)
+            {
+                sb.AppendFormat("{0} ", GetTextForNumber(i));
+            }
+            return sb.ToString();
+        }
+    }
+}
--- CodeDojo/CodeDojo.Tests/FizzBuzzTests.cs
+++ CodeDojo/CodeDojo.Tests/FizzBuzzTests.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Text;
+using CodeDojo.Kata;
 using NUnit.Framework;
 
 namespace CodeDojo.Tests
@@ -80,43 +80,5 @@ namespace CodeDojo.Tests
         }
 
         
-    }
-
-    public class FizzBuzz
-    {
-    
-        public static bool IsDivisibleByThree(int input)
-        {
-            return input%3 == 0;
-        }
-
-        public static bool IsDivisibleByFive(int input)
-        {
-            return input%5 == 0;
-        }
-
-        public string GetTextForNumber(int input)
-        {
-            if (IsDivisibleByThree(input) && IsDivisibleByFive(input))
-                return "FizzBuzz";
-
-            if (IsDivisibleByThree(input))
-                return "Fizz";
-
-            if (IsDivisibleByFive(input))
-                return "Buzz";
-
-            return input.ToString();
-        }
-
-        public string Print(int input)
-        {
-            var sb = new StringBuilder();
-            for (int i = 1; i <= input; i++)
-            {
-                sb.AppendFormat("{0} ", GetTextForNumber(i));
-            }
-            return sb.ToString();
-        }
     }
 }
