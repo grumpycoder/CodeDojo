da08b032bda7d540c4d0accb7f38f3687a6db76b
Mark Lawrence <grumpycoder@outlook.com>
2015-08-08 22:53:53 -0500

    StringCalculator tests

---
+++ CodeDojo/CodeDojo.Tests/StringCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeDojo.Tests
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var items = numbers.Split(',', '\n');
+
+            if (items.Any(x => string.IsNullOrEmpty(x)))
+                throw new ArgumentException();
+
+            var integers = items.Select(x => int.Parse(x));
+            var negatives = integers.Where(x => x < 0);
+            if (negatives.Any())
+            {
+                var message = "Negatives not allowed: {0}";
+                throw new ArgumentOutOfRangeException(string.Format(message,
+                    string.Join(",", negatives.Select(x => x.ToString()).ToArray())));
+            }
+
+            return items.Select(int.Parse).Sum();
+
+        }
+    }
+}
---
+++ CodeDojo/CodeDojo.Tests/StringCalculatorTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.CodeDom;
+using NUnit.Core;
+using NUnit.Framework;
+using CodeDojo.Kata;
+
+namespace CodeDojo.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void GivenAnEmptyStringShouldReturnZero()
+        {
+            var calculator = new StringCalculator();
+            var expected = 0;
+
+            Assert.AreEqual(expected, calculator.Add(string.Empty));
+        }
+
+        [Test]
+        public void GivenASingleNumberShouldReturnNumber()
+        {
+            var calculator = new StringCalculator();
+            var expected = 1;
+
+            Assert.AreEqual(expected, calculator.Add("1"));
+        }
+
+        [Test]
+        public void GivenTwoNumbersShouldReturnTheSumOfBothNumbers()
+        {
+            var calculator = new StringCalculator();
+            var expected = 3;
+
+            Assert.AreEqual(expected, calculator.Add("1,2"));
+        }
+
+        [Test]
+        public void GivenMultipleNumbersShouldReturnSumOfAllNumbers()
+        {
+            var calculator = new StringCalculator();
+            var expected = 6;
+
+            Assert.AreEqual(expected, calculator.Add("1,2,3"));
+        }
+
+        [Test]
+        public void GivenNewLineAsDelimeterShouldReturnSumOfAllNumbers()
+        {
+            var calculator = new StringCalculator();
+            var expected = 6;
+
+            Assert.AreEqual(expected, calculator.Add("1\n2\n3"));
+        }
+
+        [Test]
+        public void GivenConsecutiveDelimetersShouldThrowException()
+        {
+            var calculator = new StringCalculator();
+            var expected = -1;
+
+            Assert.Throws(typeof(ArgumentException), () => calculator.Add(",,"));
+        }
+
+        [Test]
+        public void GivenANegativeNumberShouldReturnThrowAnException()
+        {
+            var calculator = new StringCalculator();
+            var expected = -1;
+
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => calculator.Add("-1,2"));
+
+        }
+
+        [Test]
+        public void NegativeNumberErrorMessageShouldContainNegativeNumber()
+        {
+            var calculator = new StringCalculator();
+
+            var error = Assert.Throws(typeof(ArgumentOutOfRangeException), () => calculator.Add("-1,2"));
+            Assert.True(error.Message.Contains("-1"));
+        }
+
+    }
+}
